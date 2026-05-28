68029d87edb10ef2e31ad5716957f5e2559df2e3
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2026-05-27 21:47:12 -0500

    Create CronProvider.php

---
+++ src/Container/CronProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Cron\Container;
+
+use Ghostwriter\Container\Interface\BuilderInterface;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Service\Provider\AbstractProvider;
+use Override;
+use Throwable;
+
+/**
+ * @see CronProviderTest
+ */
+final class CronProvider extends AbstractProvider
+{
+}
